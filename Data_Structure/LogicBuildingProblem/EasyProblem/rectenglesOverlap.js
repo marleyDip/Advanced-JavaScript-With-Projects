44fba7f028d5c2129d804f0d892863f1fc21f1ad
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-05-17 19:12:46 +0600

    Find if two rectangles overlap

--- Data_Structure/LogicBuildingProblem/EasyProblem/rectenglesOverlap.js
+++ Data_Structure/LogicBuildingProblem/EasyProblem/rectenglesOverlap.js
@@ -1,3 +1,13 @@
+/* 
+    Given two rectangles, find if the given two rectangles overlap or not.
+    
+    Note that a rectangle can be represented by two coordinates, top left and bottom right. So mainly we are given following four coordinates. 
+
+        l1: Top Left coordinate of first rectangle. 
+        r1: Bottom Right coordinate of first rectangle. 
+        l2: Top Left coordinate of second rectangle. 
+        r2: Bottom Right coordinate of second rectangle. */
+
 class Point {
   constructor(x, y) {
     this.x = x;
